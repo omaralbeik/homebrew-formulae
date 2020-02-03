5e0f53c131f446ddc67eaf1f8cd15d46f4521711
Omar Albeik <omaralbeik@gmail.com>
2020-02-03 23:39:20 +0100

    Update sketchgen.rb

--- Formula/sketchgen.rb
+++ Formula/sketchgen.rb
@@ -1,20 +1,14 @@
 class Sketchgen < Formula
   desc "Command-line tool for generating source code from sketch files"
   homepage "https://github.com/omaralbeik/SketchGen"
-  url "https://github.com/omaralbeik/SketchGen"
+  url "https://github.com/omaralbeik/SketchGen.git", tag: "0.1.0"
   version "0.1.0"
   sha256 "c41ba90ec1faf06b92d87aa5cd9ae6161002ce4026553e5b9b9014e13f4895c3"
 
+  depends_on :macos
+  depends_on :xcode
+  
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", ".", "install", "PREFIX=#{prefix}"
-  end
-
-  test do
-    system "#{bin}/sketchgen" "import Foundation\n"
+    system "make", "install", "prefix=#{prefix}"
   end
 end
